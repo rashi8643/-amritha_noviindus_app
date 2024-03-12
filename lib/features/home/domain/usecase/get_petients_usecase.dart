cf777dff0fd8212586eca01db015c77d04d9993c
rashi8643 <rashimuhammed86@gmail.com>
2024-03-12 14:04:37 +0530

    domain layer usecase implemented

---
+++ lib/features/home/domain/usecase/get_petients_usecase.dart
@@ -0,0 +1,15 @@
+import 'package:novi_indus_test/features/home/domain/entity/patient_entity.dart';
+import 'package:novi_indus_test/features/home/domain/repository/api_repository.dart';
+
+final class GetPatintsUsecase {
+  final ApiRepository repository;
+  GetPatintsUsecase({required this.repository});
+
+  Future<List<PatientEntity>?> call() async {
+    try {
+      return repository.getDatas();
+    } catch (e) {
+      throw Exception('Error');
+    }
+  }
+}
